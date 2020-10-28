8b25021d1434ea611c232f9b7be494c42d071eea
Nevm PC <Nevm PC@DESKTOP-EUP8F13>
2020-10-28 09:23:27 +0200

    FIRSR COMITВ

--- queries.js
+++ queries.js
@@ -1,9 +1,9 @@
 const Pool = require('pg').Pool
 const pool = new Pool({
- host: 'localhost',
-  database: 'housedb',
-  user:'mit21',
-  password: 'mit21',
+ host: 'ec2-54-247-79-178.eu-west-1.compute.amazonaws.com',
+  database: 'd4am615tqn7fq3',
+  user:'ugrhebsleflarf',
+  password: 'b1a58307a65281150d163559af0d8b3ede580b24952c424cbb738d5d48778699',
   port: 5432,
   ssl:{rejectUnauthorized: false}
 }) 
