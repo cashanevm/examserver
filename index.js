90391abb5472ba305c0dfe716491411d16a0963d
Nevm PC <Nevm PC@DESKTOP-EUP8F13>
2020-10-28 10:18:28 +0200

    FIRSR COMITВ

--- index.js
+++ index.js
@@ -12,6 +12,16 @@ app.use(
   })
 )
 
+app.use(function(req,res,next){
+    res.header("Access-Control-Allow-Origin",'*');
+    res.header("Access-Control-Allow-Credentials",true);
+    res.header('Access-Control-Allow-Methods','GET,PUT,POST,DELETE,OPTIONS');
+    res.header("Access-Control-Allow-Headers",'Origin,X-Requested-With,Content-Type,Accept,content-type,application');
+    next();
+});
+
+
+
 app.get('/', (request, response) => {
   response.json({ info: 'Node.js, Express, and Postgres API' })
 })
